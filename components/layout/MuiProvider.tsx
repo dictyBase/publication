0bfe57124d0a6406786ea563bb1b17b8c47896d4
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-21 15:51:52 -0600

    feat: add mui provider component with custom styles

---
+++ components/layout/MuiProvider.tsx
@@ -0,0 +1,68 @@
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core"
+
+const muiTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#004080",
+      light: "#476ab0",
+      dark: "#001b53",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#008080",
+      light: "#4cb0af",
+      dark: "#005354",
+    },
+    error: {
+      main: "#b2102f",
+      light: "#ea4f58",
+      dark: "#7b0008",
+    },
+  },
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+    h1: {
+      fontSize: "2.00em",
+    },
+    h2: {
+      fontSize: "1.50em",
+    },
+    h3: {
+      fontSize: "1.17em",
+    },
+    h4: {
+      fontSize: "1.00em",
+    },
+    h5: {
+      fontSize: "0.83em",
+    },
+    h6: {
+      fontSize: "0.67em",
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        a: {
+          textDecoration: "none",
+          color: "#004080",
+          "&:hover": {
+            color: "#001b53",
+          },
+        },
+      },
+    },
+  },
+})
+
+interface Props {
+  children: React.ReactNode
+}
+
+const MuiProvider = ({ children }: Props) => {
+  return <MuiThemeProvider theme={muiTheme}>{children}</MuiThemeProvider>
+}
+
+export default MuiProvider
